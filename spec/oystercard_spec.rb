da153f18c944bfa39766be67a02fb07db44621c5
muge <muge.hasilci@hotmail.com>
2018-10-08 14:32:59 +0100

    balance added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
 describe Oystercard do
-  
+
+  describe '#balance' do
+    it 'shows balance of zero' do
+      oystercard = Oystercard.new
+      expect(subject.balance).to eq(0)
+    end
+  end
+
 end
